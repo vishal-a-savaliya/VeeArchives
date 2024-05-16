4ebd341b0d0edbada364362affc6518366df52ae
vishal savaliya <vishandsavaliya@gmail.com>
2024-05-16 17:12:40 +0530

    Quartz sync: May 16, 2024, 5:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        body: "Bricolage Grotesque",
         code: "IBM Plex Mono",
       },
       colors: {
